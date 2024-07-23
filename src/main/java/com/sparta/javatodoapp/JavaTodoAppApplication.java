d7cdb5dd9b4cc8476546f8996a027854d4d753d5
lazzzykim <33sunghyeon@gmail.com>
2024-07-23 15:39:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/sparta/javatodoapp/JavaTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.javatodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/javatodoapp/JavaTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.javatodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
